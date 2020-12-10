b279b1856c0e5b342c694d7f1caeef93fa718eba
breno-almeida <breno9985260490@gmail.com>
2020-12-10 17:11:04 -0300

    Functions of CRUD to tweet Controller

--- start/routes.js
+++ start/routes.js
@@ -1,5 +1,7 @@
 "use strict";
 
+
+
 /*
 |--------------------------------------------------------------------------
 | Routes
@@ -18,4 +20,6 @@ const Route = use("Route");
 Route.post("/register", "AuthController.register");
 Route.post("/authenticate", "AuthController.authenticate");
 
-Route.get("/app", "AppController.index").middleware(["auth"]);
+Route.group(() => {
+    Route.resource('tweets', 'TweetController').apiOnly();
+}).middleware('auth');
